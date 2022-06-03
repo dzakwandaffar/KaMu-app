1efcbbee3fae5626a6e5b51a74e0dabe05ac2a8c
Dzakwan Daffa Ramdhana <99236283+dzakwandaffar@users.noreply.github.com>
2022-06-03 07:07:01 +0700

    Initial commit

---
+++ Kamu-CC/src/routes.js
@@ -0,0 +1,18 @@
+const routes = [
+  {
+    /*
+        method: '',
+        path: '',
+        handler: ,
+        */
+  },
+  {
+    /*
+        method: '',
+        path: '',
+        handler: ,
+        */
+  },
+];
+
+module.exports = routes;
